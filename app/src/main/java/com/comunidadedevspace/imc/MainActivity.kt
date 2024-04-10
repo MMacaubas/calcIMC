433d50d3862c08b2c63e6fa7f5ff836adf1bb6c6
Monique Macaubas <moniquemacaubas@gmail.com>
2024-04-10 10:48:23 +0100

    Melhorando experiência do usuário.

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,29 +16,34 @@ class MainActivity : AppCompatActivity() {
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
 
-        val btnCalcular = findViewById<Button>(R.id.butn_calcular)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
 
             val pesoStr: String = edtPeso.text.toString()
             val alturaStr: String = edtAltura.text.toString()
 
-            if (pesoStr == "" || alturaStr == "")
+            if (pesoStr == "" || alturaStr == "") {
                 Snackbar
                     .make(
                         edtPeso,
                         "Preencha os campos corretamente",
                         Snackbar.LENGTH_LONG
                     ).show()
-        }else{
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+            } else {
 
-            val alturaQ2 = altura *+ altura
-            val resultado = peso / alturaQ2
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
 
-            println()
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
 
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra ("KEY_RESULT_IMC", resultado)
+                startActivity(intent)
+
+                println(resultado)
+            }
         }
     }
 }
